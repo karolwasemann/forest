05a367d02a011a6af8a96459bb1adce0f1435ebd
karolwasemann <kw.kwaq@gmail.com>
2021-05-18 08:27:41 +0200

    done

--- index.js
+++ index.js
@@ -3,4 +3,23 @@ import './style.css';
 const images = {
   treasure: '💰',
   leafs: '🍃'
-}
+};
+
+const trees = document.querySelectorAll('.tree');
+
+trees.forEach(tree => {
+  tree.addEventListener('mouseover', () => {
+    if (tree.classList.contains('treasure')) {
+      tree.innerHTML = images.treasure;
+      goal();
+    } else tree.innerHTML = images.leafs;
+  });
+});
+
+const goal = () => {
+  const div = document.createElement('div');
+  div.innerHTML = `Gratulacje! Znalazłeś skarb ${images.treasure}`;
+  div.style.fontSize = '24px';
+  div.style.fontFamily = 'Lato';
+  document.body.appendChild(div);
+};
