7c5cfd99343155f132d48402a56c3b99c8c031dc
karolwasemann <kw.kwaq@gmail.com>
2021-06-28 10:45:14 +0200

    done

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import './style.css';
+import { Howl, Howler } from 'howler';
 
 const images = {
   treasure: '💰',
   leafs: '🍃'
 };
 
+const sound = new Howl({
+  src: ['https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3']
+});
+
 const forest = document.querySelector('.entry');
 
 forest.addEventListener('mouseover', event => {
   if (event.target.className.includes('treasure')) {
     event.target.innerHTML = images.treasure;
-    return alert('Congratulations you win');
+    goal();
   } else event.target.innerHTML = images.leafs;
 });
+
+function goal() {
+  const message = document.createElement('p');
+  message.innerHTML = 'Congratulations you win';
+  document.body.appendChild(message);
+
+  sound.play();
+  setTimeout(() => {
+    sound.stop();
+    alert('Congratulations you win');
+  }, 3000);
+}
